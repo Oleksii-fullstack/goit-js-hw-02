92b22e98c5a8ed7da93322a98e2e2c45cfaa37a0
Oleksii Yakovliev <aleksoyak@gmail.com>
2025-04-09 18:30:34 +0300

    update task-4 file

--- js/task-4.js
+++ js/task-4.js
@@ -1,20 +1,15 @@
 
 function getShippingCost(country) {
     country = String(country);
-    let price;
     switch (country) {
         case "China":
-            price = country === "China" ? 100 : "Sorry, there is no delivery to your country";
-            return `Shipping to ${country} will cost ${price} credits`
+            return `Shipping to ${country} will cost 100 credits`
         case "Chile":
-            price = country === "Chile" ? 250 : "Sorry, there is no delivery to your country";
-            return `Shipping to ${country} will cost ${price} credits`
+            return `Shipping to ${country} will cost 250 credits`
         case "Australia":
-            price = country === "Australia" ? 170 : "Sorry, there is no delivery to your country";
-            return `Shipping to ${country} will cost ${price} credits`
+            return `Shipping to ${country} will cost 170 credits`
         case "Jamaica":
-            price = country === "Jamaica" ? 120 : "Sorry, there is no delivery to your country";
-            return `Shipping to ${country} will cost ${price} credits`
+            return `Shipping to ${country} will cost 120 credits`
         default:
             return `Sorry, there is no delivery to your country`
     }
